44df78d6ecf0fc76d4018065bf63d02cd1b094f8
Aaron Kromer <aaron.kromer@gmail.com>
2014-06-13 08:52:12 -0400

    WIP: See if this makes Travis happy.

--- spec/sanity_check_spec.rb
+++ spec/sanity_check_spec.rb
@@ -1,29 +1,35 @@
 require 'spec_helper'
 
-describe "Verify required rspec dependencies", :isolated_directory => true do
+RSpec.describe "Verify required rspec dependencies" do
+
+  tmp_root = RSpec::Core::RubyProject.root.join("tmp")
+
+  before{ FileUtils.mkdir_p tmp_root }
 
   it "fails when libraries are not required" do
-    File.write "fail_sanity_check", <<-EOF.gsub(/^\s+\|/, '')
+    script = tmp_root.join("fail_sanity_check")
+    File.write script, <<-EOF.gsub(/^\s+\|/, '')
       |#!/usr/bin/env ruby
       |RSpec::Support.require_rspec_core "project_initializer"
     EOF
-    FileUtils.chmod "+x", "fail_sanity_check"
+    FileUtils.chmod "+x", script
 
-    expect(`bundle exec fail_sanity_check 2>&1`).
+    expect(`bundle exec #{script} 2>&1`).
       to match /undefined method `require_rspec_core' for RSpec::Support:Module/
     expect($?.exitstatus).to eq 1
   end
 
   it "passes when libraries are required" do
-    File.write "pass_sanity_check", <<-EOF.gsub(/^\s+\|/, '')
+    script = tmp_root.join("pass_sanity_check")
+    File.write script, <<-EOF.gsub(/^\s+\|/, '')
       |#!/usr/bin/env ruby
       |require 'rspec/core'
       |require 'rspec/support'
       |RSpec::Support.require_rspec_core "project_initializer"
     EOF
-    FileUtils.chmod "+x", "pass_sanity_check"
+    FileUtils.chmod "+x", script
 
-    expect(`bundle exec pass_sanity_check 2>&1`).to be_empty
+    expect(`bundle exec #{script} 2>&1`).to be_empty
     expect($?.exitstatus).to eq 0
   end
 
